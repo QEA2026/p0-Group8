1aa781d32cc9de4f1e5dcb763059668c3f77bcbd
WuLandon <landonwu@berkeley.edu>
2026-06-20 11:45:43 -0700

    connect manager app to DB - initialized empty DB to test connection

--- manager-app/src/main/java/com/revature/expensemanager/config/DatabaseConfig.java
+++ manager-app/src/main/java/com/revature/expensemanager/config/DatabaseConfig.java
@@ -1,5 +1,20 @@
 package com.revature.expensemanager.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConfig {
-    
+    private static final String DB_URL = "jdbc:sqlite:../database/expense_manager.db";
+
+    public static Connection getConnection() throws SQLException {
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("SQLite JDBC driver not found.", e);
+        }
+
+        return DriverManager.getConnection(DB_URL);
+    }
 }
