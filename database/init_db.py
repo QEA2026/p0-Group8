a713ab94c2c3e01648ad05893bca3e730d73559a
Brian Paul <brianjpaul02@gmail.com>
2026-06-24 18:02:57 -0700

    Added /database tools: init_db.py (Runs schema.sql and seed.sql). Added test data to seed.sql

---
+++ database/init_db.py
@@ -0,0 +1,25 @@
+import sqlite3
+import os
+
+db_name = "database.db"
+
+# Connect to the database
+conn = sqlite3.connect(db_name)
+cursor = conn.cursor()
+
+# Run schema sql
+if os.path.exists("schema.sql"):
+    with open("schema.sql", "r") as f:
+        cursor.executescript(f.read())
+    print("Initialized database from schema.sql")
+
+# Run seed.sql
+if os.path.exists("seed.sql"):
+    with open("seed.sql", "r") as f:
+        cursor.executescript(f.read())
+    print("Ran seed.sql")
+
+# Commit changes and close the connection
+conn.commit()
+conn.close()
+print("Database initialized and seeded.")
--- database/seed.sql
+++ database/seed.sql
@@ -1,3 +1,30 @@
 /*
     We can use this file for test data I havent done anything yet :D
 */
+
+-- 1. Wipe out test data from before in reverse dependency order
+DELETE FROM approvals;
+DELETE FROM expenses;
+DELETE FROM users;
+
+-- 2. Insert test users
+INSERT INTO users (username, password, role) VALUES
+('brian', 'password','Employee'),
+('landon', 'passwerd','Employee'),
+('siri', 'password','Manager');
+
+
+-- 3. Insert Expenses
+INSERT INTO expenses (userId, amount, description, category, date) VALUES
+(3, 199.99, 'A desk','Utilities', '2026-06-19'),
+(1, 57.77, 'Poke-buzz-balls', 'Food', '2026-06-20'),
+(2, 23.88, 'Chipotle lunch w/ guacamole', 'Food', '2026-06-23'),
+(3, 49.99, 'Database streaming service expense', 'Entertainment', '2026-06-24');
+
+
+--4. Insert Approvals
+INSERT INTO approvals (expenseId, status, reviewer, comment, review_date) VALUES
+(1, 'approved', 3, 'Approved, needs a desk to work at work.', '2026-06-19'),
+(2,'pending', NULL, NULL, NULL ),
+(3, 'denied', 3, 'Denied. Missing itemized receipt.', '2026-06-24'),
+(4,'approved',3, 'Approved, cannot miss the latest Data Island episodes.', '2026-06-24');
