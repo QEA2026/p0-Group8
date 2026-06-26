51f5fa38705249b64d942a27bec740c79eab7b21
WuLandon <landonwu@berkeley.edu>
2026-06-26 16:00:45 -0700

    feat(DAOs): add DAOs for manager app - UserDAO for login - ExpenseDAO for viewing pending expenses - ApprovalDAO for approve/deny/comment submitted expenses

--- manager-app/src/main/java/com/revature/expensemanager/dao/ExpenseDao.java
+++ manager-app/src/main/java/com/revature/expensemanager/dao/ExpenseDao.java
@@ -1,5 +1,14 @@
+
 package com.revature.expensemanager.dao;
 
-public class ExpenseDao {
-    
+import java.util.List;
+import java.util.Optional;
+
+import com.revature.expensemanager.model.Expense;
+
+public interface ExpenseDAO {
+
+    Optional<Expense> findById(int id);
+
+    List<Expense> findPendingExpenses();
 }
--- manager-app/src/main/java/com/revature/expensemanager/dao/UserDao.java
+++ manager-app/src/main/java/com/revature/expensemanager/dao/UserDao.java
@@ -1,5 +1,12 @@
 package com.revature.expensemanager.dao;
 
-public class UserDao {
-    
+import java.util.Optional;
+
+import com.revature.expensemanager.model.User;
+
+public interface UserDAO {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findById(int id);
 }
